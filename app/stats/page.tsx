62ab1c4e9f1fec303cb9588aebdd64a19b3e8226
MuhammadIqbalAlghifari <iqbalalghifari18283@gmail.com>
2024-06-11 01:48:50 +0700

    updatedddddddddddd

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -1,17 +1,9 @@
 import StatCard from "@/components/StatCard";
 import { fetchUsers } from "../(auth)/actions/fetchUsers";
-import { GetServerSideProps } from "next";
 
-export const getServerSideProps: GetServerSideProps = async () => {
+const Page = async () => {
   const currentUser = await fetchUsers();
-  return {
-    props: {
-      currentUser: currentUser?.data || null,
-    },
-  };
-};
 
-const Page = ({ currentUser }: { currentUser: any }) => {
   if (!currentUser) {
     return <div>Error loading user data</div>;
   }
@@ -19,20 +11,20 @@ const Page = ({ currentUser }: { currentUser: any }) => {
   return (
     <div className="py-20">
       <div className="text-center mb-10 text-2xl uppercase">
-        <h1>{currentUser.user.username} Statistik 📊</h1>
+        <h1>{currentUser.data.user.username} Statistik 📊</h1>
       </div>
       <div className="max-w-[1500px] mx-auto w-[90%] grid sm:grid-cols-3 gap-10 justify-center">
         <StatCard
           title="Total Points"
-          value={currentUser.quizResults[0].quizScore}
+          value={currentUser.data.quizResults[0].quizScore}
         />
         <StatCard
           title="Jawaban Benar"
-          value={currentUser.quizResults[0].correctAnswers}
+          value={currentUser.data.quizResults[0].correctAnswers}
         />
         <StatCard
           title="Jawaban Salah"
-          value={currentUser.quizResults[0].wrongAnswers}
+          value={currentUser.data.quizResults[0].wrongAnswers}
         />
       </div>
     </div>
