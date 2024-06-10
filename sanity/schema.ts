151f8adc2fbed744c55f8f0b13c30fb6800215a4
MuhammadIqbalAlghifari <iqbalalghifari18283@gmail.com>
2024-06-11 03:47:08 +0700

    updateedddddddddddd

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,6 +1,6 @@
 import { type SchemaTypeDefinition } from 'sanity'
-import question from './question'
+import questions from './questions'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [question],
+  types: [questions],
 }
